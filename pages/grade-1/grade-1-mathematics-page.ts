74a55afb3bef095d7784c6d42507f76c89ba248b
Benjamin Zwicker <37882525+benjaminzwicker@users.noreply.github.com>
2024-05-16 23:38:33 -0300

    Scroll link test (#6)

--- pages/grade-1/grade-1-mathematics-page.ts
+++ pages/grade-1/grade-1-mathematics-page.ts
@@ -4,6 +4,9 @@ import BasePage from "../base-page";
 export default class GradeOneMathematicsPage extends BasePage {
   readonly urlPath: string;
   readonly gradeOneBreadcrumbLink: Locator;
+  readonly resourcesForLearningLink: Locator;
+  readonly resourcesForLearningHeader: Locator;
+  readonly resourcesForLearningParagraph: Locator;
 
   constructor(page: Page) {
     super(page);
@@ -11,6 +14,16 @@ export default class GradeOneMathematicsPage extends BasePage {
     this.gradeOneBreadcrumbLink = page.getByRole("link", {
       name: "Grade 1",
       exact: true,
+    });
+    this.resourcesForLearningLink = page.getByRole("link", {
+      name: "Resources for Learning",
+      exact: true,
+    });
+    this.resourcesForLearningHeader = page.getByRole("heading", {
+      name: "Resources for Learning",
+    });
+    this.resourcesForLearningParagraph = page.getByRole("paragraph", {
+      name: "Chosen by TVO educators, these resources support the curriculum outlined above.",
     });
   }
 
@@ -20,5 +33,9 @@ export default class GradeOneMathematicsPage extends BasePage {
 
   async clickGradeOneBreadCrumbLink() {
     await this.gradeOneBreadcrumbLink.click();
+  }
+
+  async clickResourcesForLearningLink() {
+    await this.resourcesForLearningLink.click();
   }
 }
--- tests/grade-1/mathematics/grade-1-mathematics-breadcrumbs.spec.ts
+++ tests/grade-1/mathematics/grade-1-mathematics-breadcrumbs.spec.ts
@@ -1,24 +1,51 @@
 import test, { expect } from "../../../fixtures/test-fixture";
 
-test.describe("Grade One Mathematics", () => {
-  test(
-    "Clicking on 'Grade One' from the grade one mathematics breadcrumbs should navigate back to the Grade One page",
-    { tag: "@functional" },
-    async ({ gradeOneMathematicsPage, gradeOnePage }) => {
-      await gradeOneMathematicsPage.clickGradeOneBreadCrumbLink();
-      /**
-       * This expect may seem trivial since gradeOnePage.focusOnGradeOneHeading
-       * is located by that exact text, but that could change in the future so
-       * it is best to not use .toBeVisible() under that assumption.
-       *
-       * Additionally, I prefer to have the actual expected text in the test
-       * file so that is easily readable what is being asserted.
-       */
-      expect
-        .soft(gradeOnePage.focusOnGradeOneHeading)
-        .toHaveText("Focus on Grade 1");
-      expect.soft(gradeOnePage.page).toHaveTitle("Grade 1 | TVO Learn");
-      expect(gradeOnePage.page).toHaveURL("/pages/grade-1");
-    }
-  );
-});
+test.describe(
+  "Grade One Mathematics",
+  { tag: ["@grade-1", "@mathematics"] },
+  () => {
+    test(
+      "Clicking on 'Grade One' from the grade one mathematics breadcrumbs should navigate back to the Grade One page",
+      { tag: "@functional" },
+      async ({ gradeOneMathematicsPage, gradeOnePage }) => {
+        await gradeOneMathematicsPage.clickGradeOneBreadCrumbLink();
+        /**
+         * This expect may seem trivial since gradeOnePage.focusOnGradeOneHeading
+         * is located by that exact text, but that could change in the future so
+         * it is best to not use .toBeVisible() under that assumption.
+         *
+         * Additionally, I prefer to have the actual expected text in the test
+         * file so that is easily readable what is being asserted.
+         * 
+         * We also use expect.soft() here since we are at the end of the test
+         * and no further functionality depends on these assertions passing.
+         * I would prefer to know which of these failed even after one has
+         * already failed.
+         */
+        expect
+          .soft(gradeOnePage.focusOnGradeOneHeading)
+          .toHaveText("Focus on Grade 1");
+        expect.soft(gradeOnePage.page).toHaveTitle("Grade 1 | TVO Learn");
+        expect(gradeOnePage.page).toHaveURL("/pages/grade-1");
+      }
+    );
+
+    test(
+      "Clicking on 'Resources for Learning' from the 'On this page' section should navigate to the 'Resources for Learning' heading",
+      { tag: "@functional" },
+      async ({ gradeOneMathematicsPage }) => {
+        await gradeOneMathematicsPage.clickResourcesForLearningLink();
+        /**
+         * Here we specify that we want the header to be in the viewport to verify
+         * that the page scrolled to the header as expected
+         */
+        expect
+          .soft(gradeOneMathematicsPage.resourcesForLearningHeader)
+          .toBeInViewport();
+        expect(
+          gradeOneMathematicsPage.resourcesForLearningHeader
+        ).toBeInViewport();
+      }
+    );
+  }
+);
